749620e37e534a08d2ac6575ece9b13a0ac526f3
teevik <teemu.vikoren@gmail.com>
2023-04-18 16:49:09 +0000

    aa

--- systems/x86_64-linux/x1/default.nix
+++ systems/x86_64-linux/x1/default.nix
@@ -3,7 +3,10 @@
   imports = [ ./hardware.nix ];
 
   boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi = {
+    canTouchEfiVariables = true;
+    efiSysMountPoint = "/boot/efi";
+  };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "22.11";
