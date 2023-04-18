e9790f5ac9e9b23bdf012d35de6136be42e94013
teevik <teemu.vikoren@gmail.com>
2023-04-18 17:53:35 +0000

    aa

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -52,9 +52,9 @@
           userName = "teevik";
         };
 
-        alacritty = {
-          enable = true;
-        };
+        # alacritty = {
+        #   enable = true;
+        # };
       };
     };
 
