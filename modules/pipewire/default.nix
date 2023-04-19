75be6df3ff47cd840c39104d82c1810dd7c60496
teevik <teemu.vikoren@gmail.com>
2023-04-20 00:12:44 +0200

    Pipewire

---
+++ modules/pipewire/default.nix
@@ -0,0 +1,12 @@
+{ inputs, ... }:
+{
+  config = {
+    security.rtkit.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+  };
+}
