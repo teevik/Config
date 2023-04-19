e4bb420b1831bff7a41164cc7594da6e4f91473b
teevik <teemu.vikoren@gmail.com>
2023-04-19 10:44:17 +0000

    Add fonts

---
+++ modules/fonts/default.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }:
+{
+  config = {
+    fonts.fonts = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      nerdfonts
+      jetbrains-mono
+      liberation_ttf
+      fira-code
+      fira-code-symbols
+      mplus-outline-fonts.githubRelease
+      dina-font
+      proggyfonts
+    ];
+  };
+}
