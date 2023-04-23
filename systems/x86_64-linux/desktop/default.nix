bbbcd6b4b40c2fb43f82ffb25ecc55c91191a649
teevik <teemu.vikoren@gmail.com>
2023-04-23 03:45:37 +0200

    aaa

--- systems/x86_64-linux/desktop/default.nix
+++ systems/x86_64-linux/desktop/default.nix
@@ -4,17 +4,23 @@ let
 in {
   imports = [ ./hardware.nix ];
 
+  services.xserver.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.opengl.enable = true;
+
   boot.kernelPackages = kernelPackages;
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi = {
     canTouchEfiVariables = true;
-    efiSysMountPoint = "/boot";
+    efiSysMountPoint = "/boot/efi";
   };
 
   hardware.nvidia.package = kernelPackages.nvidiaPackages.stable;
   hardware.nvidia.modesetting.enable = true;
 
+  programs.hyprland.nvidiaPatches = true;
+  home.wayland.windowManager.hyprland.nvidiaPatches = true;
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "22.11";
 }
--- systems/x86_64-linux/desktop/hardware.nix
+++ systems/x86_64-linux/desktop/hardware.nix
@@ -8,34 +8,33 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/e8aca4cc-c72b-4aad-bc18-674f3ec87bb0";
+    { device = "/dev/disk/by-uuid/fdbd64e1-c9a3-446a-a1c7-e8eb43e656ad";
       fsType = "ext4";
     };
 
   fileSystems."/boot/efi" =
-    { device = "/dev/disk/by-uuid/B1BD-1822";
+    { device = "/dev/disk/by-uuid/D1D1-99CD";
       fsType = "vfat";
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/3be6ae25-c0d8-45a6-a51b-06d1e24264bd"; }
-    ];
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
+  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  # high-resolution display
+#  hardware.video.hidpi.enable = lib.mkDefault true;
 }
