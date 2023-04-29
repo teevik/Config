fa04e15bbe6c6451853c6b82753c4c63fa1c1d7a
teevik <teemu.vikoren@gmail.com>
2023-04-29 14:24:37 +0200

    cachix

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -1,14 +1,22 @@
 { ... }:
 {
-  config = {    
+  config = {
     nix = {
       settings = {
         # Enable flakes and new 'nix' command
         experimental-features = "nix-command flakes";
         # Deduplicate and optimize nix store
         auto-optimise-store = true;
-        substituters = ["https://hyprland.cachix.org"];
-        trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+
+        substituters = [
+          "https://hyprland.cachix.org"
+          "https://nix-community.cachix.org/"
+
+        ];
+        trusted-public-keys = [
+          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        ];
       };
 
       gc = {
