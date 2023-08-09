d8044944422469949008a03ff8fd435941b731bd
teevik <teemu.vikoren@gmail.com>
2023-08-09 12:16:16 +0200

    disable gc

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -38,11 +38,11 @@
       ];
     };
 
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 30d";
-    };
+    # gc = {
+    #   automatic = true;
+    #   dates = "weekly";
+    #   options = "--delete-older-than 30d";
+    # };
 
     registry.default.flake = inputs.nixpkgs;
 
