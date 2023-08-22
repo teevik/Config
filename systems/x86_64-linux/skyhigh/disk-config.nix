cf2ef4c30ca87fbc961447bad0e7f1ca93654c03
teevik <teemu.vikoren@gmail.com>
2023-08-22 09:47:09 +0200

    skyhigh

---
+++ systems/x86_64-linux/skyhigh/disk-config.nix
@@ -0,0 +1,44 @@
+{ disks, ... }: {
+  disk = {
+    disk0 = {
+      device = builtins.elemAt disks 0;
+      type = "disk";
+
+      content = {
+        type = "table";
+        format = "gpt";
+
+        partitions = [
+          {
+            name = "ESP";
+            start = "1MiB";
+            end = "512MiB";
+
+            bootable = true;
+
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          }
+
+          {
+            name = "root";
+            start = "512MiB";
+            end = "100%";
+
+            part-type = "primary";
+            bootable = true;
+
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+            };
+          }
+        ];
+      };
+    };
+  };
+}
