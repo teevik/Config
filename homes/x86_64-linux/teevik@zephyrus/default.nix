9c42792c0f2c54d410bd25d80101a06bdd883a09
teevik <teemu.vikoren@gmail.com>
2023-10-20 00:33:54 +0200

    zephyrus home

--- homes/x86_64-linux/teevik@zephyrus/default.nix
+++ homes/x86_64-linux/teevik@zephyrus/default.nix
@@ -12,9 +12,15 @@
     services = {
       swayidle.enable = true;
     };
+
+    desktop.hyprland = {
+      enableHidpi = true;
+      scaling = 1.5;
+    };
   };
 
   home.packages = with pkgs; [
     jetbrains.clion
+    jetbrains.rust-rover
   ];
 }
---
+++ modules/home/services/swayidle/default.nix
@@ -0,0 +1,47 @@
+{ config, lib, pkgs, osConfig, ... }:
+let
+  inherit (lib) types mkOption mkIf;
+  cfg = config.teevik.services.swayidle;
+
+  hyprland = osConfig.programs.hyprland.finalPackage;
+  hyprctl = "${hyprland}/bin/hyprctl";
+  light = lib.getExe (pkgs.light);
+in
+{
+  options.teevik.services.swayidle = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = ''
+        Whether to enable swayidle
+      '';
+    };
+  };
+
+  config = mkIf cfg.enable {
+    services.swayidle = {
+      enable = true;
+
+      systemdTarget = "hyprland-session.target";
+
+      timeouts = [
+        {
+          timeout = 2 * 60;
+          command = "${light} -U 30";
+          resumeCommand = "${light} -A 30";
+        }
+
+        {
+          timeout = 3 * 60;
+          command = "${hyprctl} dispatch dpms off";
+          resumeCommand = "${hyprctl} dispatch dpms on";
+        }
+
+        {
+          timeout = 4 * 60;
+          command = "${hyprctl} dispatch exec 'systemctl suspend-then-hibernate'";
+        }
+      ];
+    };
+  };
+}
