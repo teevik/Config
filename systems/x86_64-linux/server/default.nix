8244c246617ceb4b342f9fd9f4fb9f4356b7e703
teevik <teemu.vikoren@gmail.com>
2024-03-17 20:50:48 +0100

    hw accel

--- systems/x86_64-linux/server/default.nix
+++ systems/x86_64-linux/server/default.nix
@@ -41,6 +41,20 @@ in
     };
   };
 
+  # https://wiki.archlinux.org/title/intel_graphics
+  boot.kernelParams = [
+    "i915.enable_guc=2"
+  ];
+
+  hardware.opengl = {
+    enable = true;
+
+    extraPackages = with pkgs; [
+      intel-media-driver
+      intel-compute-runtime
+    ];
+  };
+
   nixarr = {
     enable = true;
 
