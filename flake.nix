ea29e9991de43455abd9a4f5910a27c600f05e8f
teevik <teemu.vikoren@gmail.com>
2024-09-19 17:59:50 +0200

    fix stuff

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "teevik's NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
--- modules/home/desktop/hyprland/config.nix
+++ modules/home/desktop/hyprland/config.nix
@@ -14,9 +14,6 @@ in
 
   monitor=Unknown-1,disable
 
-  exec = touch ~/.config/hypr/monitors.conf
-  source = ~/.config/hypr/monitors.conf
-
   env = XCURSOR_SIZE,${builtins.toJSON (cursorSize * 2)}
 
   ${
