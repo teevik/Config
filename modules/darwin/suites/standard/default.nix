cf91298137152e293f1cf6353311e6de22be105e
teevik <teemu.vikoren@gmail.com>
2024-11-24 17:03:48 +0100

    skhd

--- modules/darwin/suites/standard/default.nix
+++ modules/darwin/suites/standard/default.nix
@@ -84,96 +84,96 @@ in
         }
       ];
 
-    services.yabai = {
-      enable = true;
-      enableScriptingAddition = true;
+    # services.yabai = {
+    #   enable = true;
+    #   enableScriptingAddition = true;
 
-      config = {
-        mouse_modifier = "alt";
+    #   config = {
+    #     mouse_modifier = "alt";
 
-        layout = "bsp";
-        focus_follows_mouse = "autoraise";
-        window_topmost = true;
-        window_placement = "second_child";
+    #     layout = "bsp";
+    #     focus_follows_mouse = "autoraise";
+    #     window_topmost = true;
+    #     window_placement = "second_child";
 
-        top_padding = 8;
-        bottom_padding = 8;
-        left_padding = 8;
-        right_padding = 8;
+    #     top_padding = 8;
+    #     bottom_padding = 8;
+    #     left_padding = 8;
+    #     right_padding = 8;
 
-        window_gap = 12;
-      };
+    #     window_gap = 12;
+    #   };
 
-      extraConfig = ''
-        yabai -m rule --add app="^System Preferences$" manage=off
-        yabai -m rule --add app="^Finder$" manage=off
-        yabai -m rule --add app="^Calculator$" manage=off
-        yabai -m rule --add app="^Disk Utility$" manage=off
-        yabai -m rule --add app="^Activity Monitor$" manage=off
-        yabai -m rule --add app="^Installer$" manage=off
-        yabai -m rule --add app="^System Information$" manage=off
-      '';
-    };
+    #   extraConfig = ''
+    #     yabai -m rule --add app="^System Preferences$" manage=off
+    #     yabai -m rule --add app="^Finder$" manage=off
+    #     yabai -m rule --add app="^Calculator$" manage=off
+    #     yabai -m rule --add app="^Disk Utility$" manage=off
+    #     yabai -m rule --add app="^Activity Monitor$" manage=off
+    #     yabai -m rule --add app="^Installer$" manage=off
+    #     yabai -m rule --add app="^System Information$" manage=off
+    #   '';
+    # };
 
-    services.skhd = {
-      enable = true;
-      skhdConfig = ''
-        alt - a : yabai -m window --toggle zoom-fullscreen
-        alt - space : yabai -m window --toggle float && \
-                      yabai -m window --grid 5:5:1:1:3:3
+    # services.skhd = {
+    #   enable = true;
+    #   skhdConfig = ''
+    #     alt - a : yabai -m window --toggle zoom-fullscreen
+    #     alt - space : yabai -m window --toggle float && \
+    #                   yabai -m window --grid 5:5:1:1:3:3
 
-        alt - q : yabai -m window --close
-        # alt - q: osascript -e 'tell application "System Events" to perform action "AXPress" of (first button whose subrole is "AXCloseButton") of (first window whose subrole is "AXStandardWindow") of (first process whose frontmost is true)'
+    #     alt - q : yabai -m window --close
+    #     # alt - q: osascript -e 'tell application "System Events" to perform action "AXPress" of (first button whose subrole is "AXCloseButton") of (first window whose subrole is "AXStandardWindow") of (first process whose frontmost is true)'
 
-        alt - d : open -a Launchpad
+    #     alt - d : open -a Launchpad
 
-        alt - return : open -na kitty
-        alt + shift - return : open -na kitty
+    #     alt - return : open -na kitty
+    #     alt + shift - return : open -na kitty
 
-        alt - w : open -na Safari
-        alt - f : open ~
-        alt - e : code
-        alt - backspace : Discord
-        alt - m : open -a Spotify
+    #     alt - w : open -na Safari
+    #     alt - f : open ~
+    #     alt - e : code
+    #     alt - backspace : Discord
+    #     alt - m : open -a Spotify
 
-        alt + cmd - left : yabai -m window --resize left:-40:0 --resize right:-40:0
-        alt + cmd - right : yabai -m window --resize right:40:0 --resize left:40:0
-        alt + cmd - up : yabai -m window --resize top:0:-40 --resize bottom:0:-40
-        alt + cmd - down : yabai -m window --resize bottom:0:40 --resize top:0:40
+    #     alt + cmd - left : yabai -m window --resize left:-40:0 --resize right:-40:0
+    #     alt + cmd - right : yabai -m window --resize right:40:0 --resize left:40:0
+    #     alt + cmd - up : yabai -m window --resize top:0:-40 --resize bottom:0:-40
+    #     alt + cmd - down : yabai -m window --resize bottom:0:40 --resize top:0:40
 
-        alt - 1 : yabai -m space --focus 1
-        alt - 2 : yabai -m space --focus 2
-        alt - 3 : yabai -m space --focus 3
-        alt - 4 : yabai -m space --focus 4
-        alt - 5 : yabai -m space --focus 5
-        alt - 6 : yabai -m space --focus 6
-        alt - 7 : yabai -m space --focus 7
-        alt - 8 : yabai -m space --focus 8
-        alt - 9 : yabai -m space --focus 9
-        alt - 0 : yabai -m space --focus 10
+    #     alt - 1 : yabai -m space --focus 1
+    #     alt - 2 : yabai -m space --focus 2
+    #     alt - 3 : yabai -m space --focus 3
+    #     alt - 4 : yabai -m space --focus 4
+    #     alt - 5 : yabai -m space --focus 5
+    #     alt - 6 : yabai -m space --focus 6
+    #     alt - 7 : yabai -m space --focus 7
+    #     alt - 8 : yabai -m space --focus 8
+    #     alt - 9 : yabai -m space --focus 9
+    #     alt - 0 : yabai -m space --focus 10
 
-        alt + shift - 1 : yabai -m window --space 1
-        alt + shift - 2 : yabai -m window --space 2
-        alt + shift - 3 : yabai -m window --space 3
-        alt + shift - 4 : yabai -m window --space 4
-        alt + shift - 5 : yabai -m window --space 5
-        alt + shift - 6 : yabai -m window --space 6
-        alt + shift - 7 : yabai -m window --space 7
-        alt + shift - 8 : yabai -m window --space 8
-        alt + shift - 9 : yabai -m window --space 9
-        alt + shift - 0 : yabai -m window --space 10
+    #     alt + shift - 1 : yabai -m window --space 1
+    #     alt + shift - 2 : yabai -m window --space 2
+    #     alt + shift - 3 : yabai -m window --space 3
+    #     alt + shift - 4 : yabai -m window --space 4
+    #     alt + shift - 5 : yabai -m window --space 5
+    #     alt + shift - 6 : yabai -m window --space 6
+    #     alt + shift - 7 : yabai -m window --space 7
+    #     alt + shift - 8 : yabai -m window --space 8
+    #     alt + shift - 9 : yabai -m window --space 9
+    #     alt + shift - 0 : yabai -m window --space 10
 
-        alt - up : yabai -m window --focus north
-        alt - right : yabai -m window --focus east
-        alt - down : yabai -m window --focus south
-        alt - left : yabai -m window --focus west
+    #     alt - up : yabai -m window --focus north
+    #     alt - right : yabai -m window --focus east
+    #     alt - down : yabai -m window --focus south
+    #     alt - left : yabai -m window --focus west
 
-        alt + shift - up : yabai -m window --warp north
-        alt + shift - right : yabai -m window --warp east
-        alt + shift - down : yabai -m window --warp south
-        alt + shift - left : yabai -m window --warp west
-      '';
-    };
+    #     alt + shift - up : yabai -m window --warp north
+    #     alt + shift - right : yabai -m window --warp east
+    #     alt + shift - down : yabai -m window --warp south
+    #     alt + shift - left : yabai -m window --warp west
+    #   '';
+    # };
 
     teevik = {
       desktop = {
@@ -191,7 +191,7 @@ in
 
     environment.systemPackages = with pkgs; [
       git
-      skhd
+      # skhd
     ];
   };
 }
