5e58a3aa871787fb506d5fe8c0701e35f686c56f
teevik <teemu.vikoren@gmail.com>
2025-03-03 21:20:46 -0500

    npu

--- hosts/zenbook/configuration.nix
+++ hosts/zenbook/configuration.nix
@@ -1,4 +1,5 @@
 {
+  perSystem,
   flake,
   inputs,
   lib,
@@ -42,40 +43,40 @@
   #   modprobe asus_nb_wmi
   # '';
 
-  services.tailscale.enable = lib.mkForce false;
-  networking.wireguard.enable = true;
-  networking.wireguard.interfaces =
-    let
-      # [Peer] section -> Endpoint
-      server_ip = "camp11.campfiresecurity.dk";
-    in
-    {
-      wg0 = {
-        # [Interface] section -> Address
-        ips = [ "10.0.240.249/32" ];
+  # services.tailscale.enable = lib.mkForce false;
+  # networking.wireguard.enable = true;
+  # networking.wireguard.interfaces =
+  #   let
+  #     # [Peer] section -> Endpoint
+  #     server_ip = "camp11.campfiresecurity.dk";
+  #   in
+  #   {
+  #     wg0 = {
+  #       # [Interface] section -> Address
+  #       ips = [ "10.0.240.249/32" ];
 
-        # [Peer] section -> Endpoint:port
-        listenPort = 5000;
+  #       # [Peer] section -> Endpoint:port
+  #       listenPort = 5000;
 
-        # Path to the private key file.
-        privateKeyFile = "/etc/campfire.key";
+  #       # Path to the private key file.
+  #       privateKeyFile = "/etc/campfire.key";
 
-        peers = [
-          {
-            # [Peer] section -> PublicKey
-            publicKey = "NUCc3FffboVY0PxhfrQB++2DY3rJUyFETabF91Eu3Vc=";
-            # [Peer] section -> AllowedIPs
-            allowedIPs = [
-              "10.42.7.0/24"
-              "10.0.240.1/32"
-            ];
-            # [Peer] section -> Endpoint:port
-            endpoint = "${server_ip}:5000";
-            persistentKeepalive = 25;
-          }
-        ];
-      };
-    };
+  #       peers = [
+  #         {
+  #           # [Peer] section -> PublicKey
+  #           publicKey = "NUCc3FffboVY0PxhfrQB++2DY3rJUyFETabF91Eu3Vc=";
+  #           # [Peer] section -> AllowedIPs
+  #           allowedIPs = [
+  #             "10.42.7.0/24"
+  #             "10.0.240.1/32"
+  #           ];
+  #           # [Peer] section -> Endpoint:port
+  #           endpoint = "${server_ip}:5000";
+  #           persistentKeepalive = 25;
+  #         }
+  #       ];
+  #     };
+  #   };
 
   boot = {
     kernelPackages = lib.mkForce pkgs.linuxPackages_testing;
@@ -85,10 +86,10 @@
     # More power savings
     # https://community.frame.work/t/tracking-linux-battery-life-tuning/6665/594
     # https://discourse.ubuntu.com/t/fine-tuning-the-ubuntu-24-04-kernel-for-low-latency-throughput-and-power-efficiency/44834
-    kernelParams = [
-      "rcu_nocbs=all"
-      "rcutree.enable_rcu_lazy=1"
-    ];
+    # kernelParams = [
+    #   "rcu_nocbs=all"
+    #   "rcutree.enable_rcu_lazy=1"
+    # ];
 
     # blacklistedKernelModules = [
     #   "asus-nb-wmi" # Eats ~0.30 W
@@ -97,21 +98,23 @@
 
   hardware.firmware = [
     pkgs.sof-firmware
-    (
-      let
-        model = "37xx";
-        version = "0.0";
+    perSystem.self.intel-npu-driver
 
-        firmware = pkgs.fetchurl {
-          url = "https://github.com/intel/linux-npu-driver/raw/v1.13.0/firmware/bin/vpu_${model}_v${version}.bin";
-          hash = "sha256-Mpoeq8HrwChjtHALsss/7QsFtDYAoFNsnhllU0xp3os=";
-        };
-      in
-      pkgs.runCommand "intel-vpu-firmware-${model}-${version}" { } ''
-        mkdir -p "$out/lib/firmware/intel/vpu"
-        cp '${firmware}' "$out/lib/firmware/intel/vpu/vpu_${model}_v${version}.bin"
-      ''
-    )
+    # (
+    #   let
+    #     model = "37xx";
+    #     version = "0.0";
+
+    #     firmware = pkgs.fetchurl {
+    #       url = "https://github.com/intel/linux-npu-driver/raw/v1.13.0/firmware/bin/vpu_${model}_v${version}.bin";
+    #       hash = "sha256-Mpoeq8HrwChjtHALsss/7QsFtDYAoFNsnhllU0xp3os=";
+    #     };
+    #   in
+    #   pkgs.runCommand "intel-vpu-firmware-${model}-${version}" { } ''
+    #     mkdir -p "$out/lib/firmware/intel/vpu"
+    #     cp '${firmware}' "$out/lib/firmware/intel/vpu/vpu_${model}_v${version}.bin"
+    #   ''
+    # )
   ];
 
   system.stateVersion = "24.11";
