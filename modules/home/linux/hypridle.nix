bfe7d366d8d66b1245edac6c524375b07dfce4dc
teevik <teemu.vikoren@gmail.com>
2026-01-28 19:07:41 +0100

    Fix hypridle

--- modules/home/linux/hypridle.nix
+++ modules/home/linux/hypridle.nix
@@ -1,40 +1,30 @@
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 let
   hyprland = pkgs.hyprland;
   hyprctl = "${hyprland}/bin/hyprctl";
-  light = lib.getExe (pkgs.light);
-
-  checkAudio =
-    command: # bash
-    ''
-      ${pkgs.pipewire}/bin/pw-cli i all 2>&1 | ${pkgs.ripgrep}/bin/rg running -q
-      # only suspend if audio isn't running
-      if [ $? == 1 ]; then
-        ${command}
-      fi
-    '';
+  light = "${pkgs.light}/bin/light";
 in
 {
   services.hypridle = {
     enable = true;
 
     settings = {
-      listeners = [
+      listener = [
         {
           timeout = 2 * 60;
-          on-timeout = checkAudio "${light} -U 50";
+          on-timeout = "${light} -U 50";
           on-resume = "${light} -A 50";
         }
 
         {
           timeout = 3 * 60;
-          on-timeout = checkAudio "${hyprctl} dispatch dpms off";
+          on-timeout = "${hyprctl} dispatch dpms off";
           on-resume = "${hyprctl} dispatch dpms on";
         }
 
         # {
         #   timeout = 4 * 60;
-        #   on-timeout = checkAudio "${hyprctl} dispatch dpms on && ${hyprctl} dispatch exec 'systemctl suspend-then-hibernate'";
+        #   on-timeout = "${hyprctl} dispatch dpms on && ${hyprctl} dispatch exec 'systemctl suspend-then-hibernate'";
         # }
       ];
     };
