2ec782dd32d5b4715645172d744d29b17c7775c3
teevik <teemu.vikoren@gmail.com>
2026-03-11 20:28:21 +0100

    Update

--- packages/opencode.nix
+++ packages/opencode.nix
@@ -1,14 +1,14 @@
 { pkgs, ... }:
 pkgs.opencode.overrideAttrs (old: rec {
-  version = "1.2.20";
+  version = "1.2.24";
   src = pkgs.fetchFromGitHub {
     owner = "anomalyco";
     repo = "opencode";
     tag = "v${version}";
-    hash = "sha256-FBmF7/uwZYY/qY1252Hz+XhXdE+Qp5axySAy5Jw7XUQ=";
+    hash = "sha256-smGIc6lYWSjfmGAikoYpP7GbB6mWacrPWrRtp/+HJ3E=";
   };
   node_modules = old.node_modules.overrideAttrs {
     inherit src;
-    outputHash = "sha256-OwlJRAeKnX5YMwQgaV4op40rjt5kxsP4WrOzpp9t90w=";
+    outputHash = "sha256-twywrmswEl687u5mqWgVVzOeWOheNGuW3e4L5tq/Qbw=";
   };
 })
