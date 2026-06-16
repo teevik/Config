7e508e3dca8416c21b40cf2e48e53b5a91c81e5b
teevik <teemu.vikoren@gmail.com>
2026-06-16 21:07:52 +0200

    Update system-configuration.nix

--- hosts/precision/system-configuration.nix
+++ hosts/precision/system-configuration.nix
@@ -1,7 +1,6 @@
 {
   inputs,
   flake,
-  perSystem,
   pkgs,
   ...
 }:
@@ -19,56 +18,25 @@
   system-manager.allowAnyDistro = true;
   system-graphics.enable = true;
 
-  nix = {
-    enable = true;
-    package = perSystem.determinate-nix.default;
-    settings = {
-      experimental-features = [
-        "nix-command"
-        "flakes"
-        "ca-derivations"
-        "dynamic-derivations"
-        "parallel-eval"
-      ];
-      auto-optimise-store = true;
-
-      trusted-users = [
-        "root"
-        "teemu.vikoeren"
-      ];
-
-      max-substitution-jobs = 128;
-      http-connections = 128;
-
-      eval-cores = 0;
-      lazy-trees = true;
-
-      keep-derivations = true;
-      keep-outputs = true;
-
-      connect-timeout = 5;
-      fallback = true;
-
-      substituters = [
-        "https://cache.nixos.org"
-        "https://teevik.cachix.org"
-        "https://hyprland.cachix.org"
-        "https://install.determinate.systems"
-      ];
-
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "desktop-1:VvIgYHAClUfjQjKWeNaCiQTRm9Q3fO0Q3v08KLTp0yo="
-        "teevik.cachix.org-1:lh2jXPvLIaTNsL8e8gvrI2abYe83tKhV0PmxQOGlitQ="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
-      ];
-    };
-  };
-
   fonts.fontconfig.enable = true;
 
   environment = {
+    etc."nix/nix.custom.conf".text = ''
+      experimental-features = nix-command flakes ca-derivations dynamic-derivations parallel-eval
+      auto-optimise-store = true
+      trusted-users = root teemu.vikoeren
+      max-substitution-jobs = 128
+      http-connections = 128
+      eval-cores = 0
+      lazy-trees = true
+      keep-derivations = true
+      keep-outputs = true
+      connect-timeout = 5
+      fallback = true
+      substituters = https://cache.nixos.org https://teevik.cachix.org https://hyprland.cachix.org https://install.determinate.systems
+      trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= desktop-1:VvIgYHAClUfjQjKWeNaCiQTRm9Q3fO0Q3v08KLTp0yo= teevik.cachix.org-1:lh2jXPvLIaTNsL8e8gvrI2abYe83tKhV0PmxQOGlitQ= hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc= cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM=
+    '';
+
     sessionVariables = {
       NIXOS_OZONE_WL = "1";
       QT_STYLE_OVERRIDE = "adwaita-dark";
